d5584b8e406da7a791aefb84ebdf3fd0db8ff31e
alxjrvs <alxjrvs@gmail.com>
2012-09-24 17:51:29 -0400

    basic index made

--- app/controllers/pool_controller.rb
+++ app/controllers/pool_controller.rb
@@ -1,7 +1,11 @@
 class PoolController < ActionController::Base
 
 def index
-
+  @pool = Release.first.gen_pool 6
+  respond_to do |format|
+    format.html #index.html.haml
+    format.json { render json: @pool }
+  end
 end
 
 end
