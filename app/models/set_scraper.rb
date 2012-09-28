09dd7fd7423244a3f5eb47e57ada407a86bce5b8
alxjrvs <alxjrvs@gmail.com>
2012-09-27 20:59:19 -0400

    Hacks firmly in place for Transform, Split, and Flip.

--- app/models/set_scraper.rb
+++ app/models/set_scraper.rb
@@ -11,83 +11,85 @@ require 'nokogiri'
   end #rarity_sort
 
 
-  def self.split_check(a, b, set)
+  def self.split_merge(hash)
+    "#{/(\w*)/.match(hash[:name]).to_s}\n#{hash[:cost]}\n#{hash[:card_type]}\n#{hash[:rules]}\n#{hash[:pow_tgh]} "
+  end
+
+  def self.split_check # For these, we don't care what comes first. Load it into @b, stich it together, godspeed.
     if @b
-      if @a[:cost] == ''
-        @b[:rules] = "#{b[:rules]}\n-----\n#{@a[:name]}\n#{@a[:card_type]}\n#{@a[:rules]}\n#{@a[:pow_tgh]}"
-        card = Card.create @b
-        set.cards << card
-        @b = nil
-        @a ={:rules => ''}
-      elsif @b[:cost] == ''
-        @b = nil
-      else
-        card = Card.create @b
-        set.cards << card
-        @b = nil
-      end
+      @b.merge!({:name => /\w* \/\/ \w*/.match(@b[:name]).to_s,
+      :cost => "#{@b[:cost]} // #{@a[:cost]}",
+      :card_type => "#{@b[:card_type]} //#{@a[:card_type]}",
+      :rules => "#{split_merge(@b)}\n-----\n#{split_merge(@a)}"}
+     )
+      card = Card.create @b
+      @set.cards << card
+      @b = nil
+      @a ={:name => ''}
     end
-    if a[:rules].include? "transform" or a[:rules].include? "Transform"
-      @b = a
+    if /\w* \/\/ \w*/.match(@a[:name])
+      binding.pry
+      @b = @a
       @a = {}
     end
   end
-  def self.flip_check(a, b, set)
-    if @b
-      if @a[:cost] == ''
-        @b[:rules] = "#{b[:rules]}\n-----\n#{@a[:name]}\n#{@a[:card_type]}\n#{@a[:rules]}\n#{@a[:pow_tgh]}"
+  def self.flip_check #In this case, it's a little easier to sort - the names of the cards appear on each other. However, the Order is  potentially different.
+    if @b #This is the secondary '@b' Loop the flipped card came first.
+      if @a[:rules].include? "#flip {@a[:name]}" or @a[:rules].include? "flip it"
+        @b[:rules] = "#{@b[:rules]}\n-----\n#{@a[:name]}\n#{@a[:card_type]}\n#{@a[:rules]}\n#{@a[:pow_tgh]}"
         card = Card.create @b
-        set.cards << card
+        @set.cards << card
         @b = nil
-        @a ={:rules => ''}
-      elsif @b[:cost] == ''
-        @b = nil
-      else
-        card = Card.create @b
-        set.cards << card
-        @b = nil
+        @a ={:name => '', :rules => ''}
       end
     end
-    if a[:name].include? "("
-      @b = a
-      @a = {}
+    if @a[:name].include? "("
+      if @a[:name].include? Card.last.name #this if group handles it if the Flip card came second.
+        Card.last.update_attributes(:rules => "#{Card.last.rules}\n-----\n#{@a[:name].gsub(Card.last.name, '')}\n#{@a[:card_type]}\n#{@a[:rules]}\n#{@a[:pow_tgh]}")
+        @a = {}
+      else
+        @b = @a
+        @a = {}
+      end
     end
   end
 
 
-  def self.transform_check(a, b, set)
+  def self.transform_check
     if @b #B represents the first half of the untransformed creature. it is set below.
       if @a[:cost] == '' #if the new card has no cost, that means it is probably the second half of a transformed creature
-        @b[:rules] = "#{b[:rules]}\n-----\n#{@a[:name]}\n#{@a[:card_type]}\n#{@a[:rules]}\n#{@a[:pow_tgh]}"
+        @b[:rules] = "#{@b[:rules]}\n-----\n#{@a[:name]}\n#{@a[:card_type]}\n#{@a[:rules]}\n#{@a[:pow_tgh]}"
         card = Card.create @b
-        set.cards << card
+        @set.cards << card
         @b = nil
-        @a ={:rules => ''}
+        @a ={:rules => '', :name => ''}
       elsif @b[:cost] == '' # Now, Some cards will trigger as having ' transform', but they'll be the other half of flip cards. This checks that and clears it out.
         @b = nil
       else #This is for everything else (read: Moonmist)
         card = Card.create @b
-        set.cards << card
+        @set.cards << card
         @b = nil
       end
     end
-    if a[:rules].include? "transform" or a[:rules].include? "Transform"
-      @b = a
+    if @a[:rules].include? "transform" or @a[:rules].include? "Transform"
+      @b = @a
       @a = {}
     end
   end
 
   def self.scrape(set_name, short_name)
-    set = Release.create(name: set_name, short_name: short_name)
+    @set = Release.create(name: set_name, short_name: short_name)
     set_url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?sort=cn+&output=spoiler&method=text&set=[%22#{set_name.gsub(/\s/, "%20")}%22]"
     doc = Nokogiri::HTML(open(set_url))
     @b = nil
     @a = {}
+    @split = false
     doc.search('table > tr').each do |row|
       if row.at_xpath('td[2]')
         case row.at_xpath('td[1]').text.strip
         when "Name:"
          @a.merge!({ name: row.at_xpath('td[2]').text.strip })
+          @split = true if @a[:name].include? '//'
         when "Cost:"
          @a.merge!({ cost: row.at_xpath('td[2]').text.strip.upcase })
         when "Type:"
@@ -102,10 +104,13 @@ require 'nokogiri'
           @a.merge!({ pow_tgh: row.at_xpath('td[2]').text.strip })
         when "Set/Rarity:"
             @a.merge!({ rarity: SetScraper.rarity_sort(set_name, row.at_xpath('td[2]').text) })
-            SetScraper.transform_check(@a, @b, set)
+            SetScraper.flip_check  if set_name.include? "Kamigawa"
+            SetScraper.split_check if @split
+            SetScraper.transform_check if short_name == "ISD" or short_name == "DKA"
             Card.create @a
+            @a = {}
             Card.last.update_attributes(:rarity => "Token") if Card.last.name.include? "token card"
-            set.cards << Card.last
+            @set.cards << Card.last
           p "#{Card.last.name } saved to #{Card.last.release.name}"
         end #case row
       end #if row.at_xpath
